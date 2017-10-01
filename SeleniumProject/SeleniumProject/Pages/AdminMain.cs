b617d15b8123c92261c43a67b81c078668d63b10
MihaZiK <mihazik.lol@mail.ru>
2017-10-01 13:52:31 +0700

    Add submenu clicker

--- SeleniumProject/SeleniumProject/Pages/AdminMain.cs
+++ SeleniumProject/SeleniumProject/Pages/AdminMain.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using System;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Support.PageObjects;
 using SeleniumProject.Wrappers;
 
@@ -17,9 +18,23 @@ namespace SeleniumProject.Pages
 
         public bool MenuChecker()
         {
+            // Идем по списку всех элементов меню
             for (var i = 0; i < WebDriver.Driver.FindElements(By.Id("app-")).Count; i++)
             {
-                BaseSelenium.Click(WebDriver.Driver.FindElements(By.Id("app-"))[i]);
+                IWebElement menuItem = WebDriver.Driver.FindElements(By.Id("app-"))[i];    
+                BaseSelenium.Click(menuItem);
+                
+                // Если есть подменю, то проходим и по нему
+                if (BaseAsserts.AreElementsPresent(By.CssSelector("ul.docs li")))
+                {
+                    for (var j = 0; j < WebDriver.Driver.FindElements(By.CssSelector("ul.docs li")).Count; j++)
+                    {
+                        menuItem = WebDriver.Driver.FindElements(By.CssSelector("ul.docs li"))[j]; 
+                        BaseSelenium.Click(menuItem);
+                        if (!BaseAsserts.IsElementsPresent(By.CssSelector("h1")))
+                            return false;
+                    }
+                }
                 if (!BaseAsserts.IsElementsPresent(By.CssSelector("h1")))
                     return false;
             }
